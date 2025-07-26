cc59dc946816555f500a059c8e7ce643dbb89b21
alburt <albert.asaftei00@gmail.com>
2025-07-26 20:38:45 +0200

    chore: :adhesive_bandage: put in WIP Dialog component for style related issues

--- src/layouts/DocsLayout.tsx
+++ src/layouts/DocsLayout.tsx
@@ -22,7 +22,7 @@ const navItems: {
     {
       name: "Dialog",
       path: routes.docs.components.dialog,
-      workInProgress: false,
+      workInProgress: true,
     },
     {
       name: "Input",
