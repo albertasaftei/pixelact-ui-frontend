e4b14b11294961bee655f15d762cb268ab55ced9
alburt <albert.asaftei00@gmail.com>
2025-07-31 00:26:02 +0200

    feat: :sparkles: added cookies banner cause we dont want to go to jail

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import "pixelact-ui/styles.css";
 import "highlight.js/styles/github-dark.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -7,6 +7,9 @@ import { DocsLayout } from "./layouts/DocsLayout";
 import Layout from "./layouts/Layout";
 import Spinner from "./components/Spinner";
 import mdxComponents from "./components/MdxComponents";
+import { PostHogProvider } from "posthog-js/react";
+import posthog from "posthog-js";
+import CookiesBanner from "./components/CookiesBanner";
 
 const LazyHomepage = React.lazy(() => import("./pages/Homepage"));
 const LazyNotFound = React.lazy(() => import("./pages/NotFound"));
@@ -36,54 +39,77 @@ const SuspenseLoader = () => (
   </div>
 );
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
+const RoutesComponent = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path={routes.root}
+        element={
+          <Suspense fallback={<SuspenseLoader />}>
+            <Layout />
+          </Suspense>
+        }
+      >
         <Route
           path={routes.root}
           element={
             <Suspense fallback={<SuspenseLoader />}>
-              <Layout />
-            </Suspense>
-          }
-        >
-          <Route
-            path={routes.root}
-            element={
-              <Suspense fallback={<SuspenseLoader />}>
-                <LazyHomepage />
-              </Suspense>
-            }
-          />
-          <Route element={<DocsLayout />}>
-            {contentRoutes.map((route) => (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={route.element}
-              />
-            ))}
-          </Route>
-          <Route
-            path={routes.showcase}
-            element={
-              <Suspense fallback={<SuspenseLoader />}>
-                <LazyShowcase />
-              </Suspense>
-            }
-          />
-        </Route>
-        <Route
-          path={routes.notFound}
-          element={
-            <Suspense fallback={<SuspenseLoader />}>
-              <LazyNotFound />
+              <LazyHomepage />
             </Suspense>
           }
         />
-      </Routes>
-    </BrowserRouter>
+        <Route element={<DocsLayout />}>
+          {contentRoutes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Route>
+        <Route
+          path={routes.showcase}
+          element={
+            <Suspense fallback={<SuspenseLoader />}>
+              <LazyShowcase />
+            </Suspense>
+          }
+        />
+      </Route>
+      <Route
+        path={routes.notFound}
+        element={
+          <Suspense fallback={<SuspenseLoader />}>
+            <LazyNotFound />
+          </Suspense>
+        }
+      />
+    </Routes>
+  </BrowserRouter>
+);
+
+function App() {
+  const [posthogClient, setPosthogClient] = useState<typeof posthog | null>(
+    null
+  );
+
+  const initPostHog = () => {
+    posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
+      api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+      disable_session_recording: true,
+      debug: import.meta.env.MODE === "development",
+      capture_exceptions: true,
+    });
+    setPosthogClient(posthog);
+  };
+
+  return (
+    <>
+      {posthogClient ? (
+        <PostHogProvider client={posthogClient}>
+          <RoutesComponent />
+        </PostHogProvider>
+      ) : (
+        <RoutesComponent />
+      )}
+      <CookiesBanner onAccept={() => initPostHog()} onDecline={() => {}} />
+    </>
   );
 }
 
